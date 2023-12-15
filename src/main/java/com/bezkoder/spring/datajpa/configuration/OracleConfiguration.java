6c8c8ea2859a887c215f27041ef7f089b5b8f6ab
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-12-15 14:07:59 +0100

    Using UCP

--- src/main/java/com/bezkoder/spring/datajpa/configuration/OracleConfiguration.java
+++ src/main/java/com/bezkoder/spring/datajpa/configuration/OracleConfiguration.java
@@ -1,14 +1,19 @@
 package com.bezkoder.spring.datajpa.configuration;
 
+import java.util.Properties;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import javax.validation.constraints.NotNull;
 
-import lombok.Setter;
-
 import oracle.jdbc.pool.OracleDataSource;
-
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceImpl;
+import oracle.ucp.jdbc.ConnectionInitializationCallback;
+import oracle.jdbc.OracleConnection;
+    
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,33 +22,61 @@ import org.springframework.data.jdbc.support.ConnectionUsernameProvider;
 import org.springframework.data.jdbc.support.oracle.ProxyDataSource;
 import org.springframework.util.Assert;
 
-    
+import org.springframework.beans.factory.annotation.Value;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Configuration
-@ConfigurationProperties("oracle")
-@Setter
 public class OracleConfiguration {
+    
+    public static final Logger logger = LoggerFactory.getLogger(OracleConfiguration.class);
+
+    static {
+        logger.info("Initializing {}", OracleConfiguration.class.toString());
+    }
+
+    private @Value("${spring.datasource.schema}") String schema;
+
     @Autowired
     private ConnectionUsernameProvider contextProvider;
-
-    @NotNull
-    private String username;
-
-    @NotNull
-    private String password;
-
-    @NotNull
-    private String url;
     
-    @Bean
-    OracleDataSource oracleDataSource() throws SQLException {
-        return OracleDataSourceFactory.getOracleDataSource(url, username, password);
+    @Bean(name = {"dataSourceProperties"})
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSourceProperties getDataSourceProperties() {
+        return new DataSourceProperties();
     }
-    
-    @Bean
+
     @Primary
-    DataSource dataSource() throws SQLException {
-        Assert.notNull(contextProvider, "Context provider must NOT be null");
+    @Bean(name = {"dataSource"})
+    @ConfigurationProperties(prefix = "spring.datasource.oracleucp")
+    public DataSource getDataSource(@Qualifier("dataSourceProperties") DataSourceProperties properties) throws java.sql.SQLException {
+        logger.info("data source properties: " + properties.toString());
         
-        return new ProxyDataSource(oracleDataSource(), contextProvider);
+        DataSource dataSource = properties
+            .initializeDataSourceBuilder()
+            .type(PoolDataSourceImpl.class)
+            .build();
+        
+        Assert.isInstanceOf(PoolDataSource.class, dataSource, "data source should be of type " + PoolDataSource.class.toString());
+
+        Assert.notNull(contextProvider, "Context provider must NOT be null");
+
+        ((PoolDataSource)dataSource).registerConnectionInitializationCallback(new ConnectionInitializationCallback() {
+                @Override
+                public void initialize(java.sql.Connection connection)  throws java.sql.SQLException {
+                    final OracleConnection oracleConnection = (OracleConnection) connection;
+
+                    if (!oracleConnection.isProxySession()) {
+                        final Properties properties = new Properties();
+
+                        properties.setProperty("PROXY_USER_NAME", schema);
+                            
+                        oracleConnection.openProxySession​(OracleConnection.PROXYTYPE_USER_NAME, properties);
+                    }
+                }
+            });
+            
+        return dataSource;
     }
 }
--- src/main/java/com/bezkoder/spring/datajpa/provider/UserProvider.java
+++ src/main/java/com/bezkoder/spring/datajpa/provider/UserProvider.java
@@ -1,22 +1,19 @@
 package com.bezkoder.spring.datajpa.provider;
 
 import javax.validation.constraints.NotNull;
-import org.springframework.stereotype.Component;
-import org.springframework.data.jdbc.support.ConnectionUsernameProvider;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.jdbc.support.ConnectionUsernameProvider;
+import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
 
 @Component
-@ConfigurationProperties("app1")
 public class UserProvider implements ConnectionUsernameProvider {
 
     @NotNull
-    private String schema;
-
-    public void setSchema(String schema) {
-        this.schema = schema;
-    }
+    private @Value("${spring.datasource.schema}") String schema;
 
     @Override
     public String getUserName() {
